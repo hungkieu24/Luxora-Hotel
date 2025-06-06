b0a7d7a212b9a612aa6e6d74bc7e916b8d708d6c
hoangnm2111 <242226hoang@gmail.com>
2025-06-07 02:32:59 +0700

    Create new editProfile to template

---
+++ src/java/Controller/Customer/EditProfileServlet1.java
@@ -0,0 +1,163 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package Controller.Customer;
+
+import Dal.LoyaltyPointDAO;
+import Dal.UserAccountDAO;
+import Model.LoyaltyPoint;
+import Model.UserAccount;
+import Utility.UploadImage;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ *
+ * @author KTC
+ */
+@WebServlet(name="EditProfileServlet1", urlPatterns={"/editProfile1"})
+@MultipartConfig(
+        fileSizeThreshold = 1024 * 1024 * 2, // 2MB
+        maxFileSize = 1024 * 1024 * 10, // 10MB
+        maxRequestSize = 1024 * 1024 * 50 // 50MB
+)
+public class EditProfileServlet1 extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet EditProfileServlet1</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet EditProfileServlet1 at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        
+        UserAccount user = (UserAccount) session.getAttribute("user");
+        if (user == null) {
+            response.sendRedirect("login.jsp");
+            return;
+        }
+
+        LoyaltyPointDAO loyaltypointdao = new LoyaltyPointDAO(); 
+        LoyaltyPoint loyaltypointlp = loyaltypointdao.getLoyaltyPointByUserId(user.getId());
+        
+        session.setAttribute("loyaltypointlp", loyaltypointlp); 
+        request.getRequestDispatcher("editProfile1.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        HttpSession session = request.getSession();
+        UserAccountDAO useraccountdao = new UserAccountDAO();
+        UserAccount user = (UserAccount) session.getAttribute("user");
+
+        UploadImage up = new UploadImage();
+        String UPLOAD_DIR = "/img/avatar";
+        
+        response.setContentType("text/html;charset=UTF-8");
+
+        // Đường dẫn lưu trữ trong dự án
+        String image = user.getAvatar_url();
+        String pathHost = getServletContext().getRealPath("");
+        String finalPath = pathHost.replace("build\\", ""); 
+        String uploadPath = finalPath + UPLOAD_DIR;
+        
+        
+        // Upload ảnh
+        String fileName = up.uploadImage(request, "avatar", uploadPath);
+        String avatarUrl ="."+ UPLOAD_DIR+"/"+fileName;
+        if (fileName == null) {
+            avatarUrl = image;
+            
+        }
+        
+        String username = request.getParameter("username");
+        String email = request.getParameter("email");
+        String phonenumber = request.getParameter("phonenumber");
+        
+        if (username != null && !username.trim().isEmpty()) {
+            session.setAttribute("username", username);
+        }
+        if (email != null && !email.trim().isEmpty()) {
+            session.setAttribute("email", email);
+        }
+        if (phonenumber != null && !phonenumber.trim().isEmpty()) {
+            session.setAttribute("phonenumber", phonenumber);
+        }
+        
+        boolean updated = useraccountdao.updateUserInfo(user.getId(), username, email, phonenumber, avatarUrl);
+        
+        if (updated) {
+            UserAccount useraccount = useraccountdao.getUserById(user.getId());
+            session.setAttribute("user", useraccount);
+            setSessionMessage(session, "Information updated successfully!","success");
+        } else {
+            setSessionMessage(session, "Update information failed!!","error");
+        }
+        
+        request.getRequestDispatcher("editProfile1.jsp").forward(request, response);
+        
+    }
+    private void setSessionMessage(HttpSession session, String message, String type) {
+        session.setAttribute("message", message);
+        session.setAttribute("messageType", type);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
